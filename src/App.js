843ba871fe2faba7471b1a210786b8e89d305812
Anton <kucherenko-anton1@mail.ru>
2023-08-09 16:21:16 +0300

    Header added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Services from './pages/blog/services';
-import Products from './pages/blog/products';
-import Posts from './pages/blog/posts';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Services from "./pages/blog/services";
+import Products from "./pages/blog/products";
+import Posts from "./pages/blog/posts";
+import Header from "./pages/blog/header/header";
+import Main from "./pages/blog/main";
+
+import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/services" element={<Services />} /> 
-        <Route path="/products" element={<Products />} /> 
-        <Route path="/posts" element={<Posts />} /> 
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/posts" element={<Posts />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
-
---
+++ src/pages/blog/header/header.js
@@ -0,0 +1,58 @@
+import "./header.css";
+
+function App() {
+  return (
+    <header>
+      <div className="nav">
+        <ul className="nav-list">
+          <li>
+            <a className="<%= path === '/' ? 'active' : '' %>" href="/">
+              Main
+            </a>
+          </li>
+          <li>
+            <a
+              className="<%= path === '/services' ? 'active' : '' %>"
+              href="/services"
+            >
+              Services
+            </a>
+          </li>
+          <li>
+            <a
+              className="<%= path === '/products' ? 'active' : '' %>"
+              href="/products"
+            >
+              Products
+            </a>
+          </li>
+          <li>
+            <a className="<%= path === '/posts' ? 'active' : '' %>" href="/posts">
+              Posts
+            </a>
+          </li>
+        </ul>
+        <ul className="nav-list">
+          <li>
+            <a className="<%= path === '/login' ? 'active' : '' %>" href="/login">
+              Login
+            </a>
+          </li>
+          <li>
+            <a className="<%= path === '/signup' ? 'active' : '' %>" href="/signup">
+              SignUp
+            </a>
+          </li>
+          <li className="main-header__item">
+            <form action="/logout" method="post">
+              <input type="hidden" name="_csrf" value="<%= csrfToken %>" />
+              <button type="submit">Logout</button>
+            </form>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default App;
